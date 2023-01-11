3de592d83649a243f557fda3fc765b0675c699c1
Gabriel Loutfalla Kanawati <gabrielkanawati3@gmail.com>
2023-01-11 15:58:42 -0300

    first commit

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,79 @@
+import { useContext } from 'react'
+import { Minus, Plus } from 'phosphor-react'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
+
+import {
+  ButtonIconMinutes,
+  FormContainer,
+  MinutesAmountContainer,
+  MinutesAmountInput,
+  TaskInput,
+} from './styles'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register, setValue, getValues } = useFormContext()
+
+  function handleDecreaseMinutes() {
+    const minutesValue = getValues('minutesAmount')
+
+    let minutesDecrease = minutesValue
+    if (minutesDecrease > 5) {
+      minutesDecrease = minutesDecrease - 5
+    }
+    setValue('minutesAmount', minutesDecrease)
+  }
+
+  function handleIncreaseMinutes() {
+    const minutesValue = getValues('minutesAmount')
+
+    let minutesIncrease = minutesValue
+    if (minutesIncrease < 60) {
+      minutesIncrease = minutesIncrease + 5
+    }
+    setValue('minutesAmount', minutesIncrease)
+  }
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        type="text"
+        placeholder="Dê um nome para o seu projeto"
+        {...register('task')}
+        disabled={!!activeCycle}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Estudar Ignite" />
+        <option value="Finalizar Projeto IgniteTimer" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+
+      <MinutesAmountContainer>
+        <ButtonIconMinutes onClick={handleDecreaseMinutes} type="button">
+          <Minus size={15} />
+        </ButtonIconMinutes>
+        <MinutesAmountInput
+          id="minutesAmount"
+          type="number"
+          placeholder="00"
+          step={5}
+          min={5}
+          max={60}
+          {...register('minutesAmount', { valueAsNumber: true })}
+          disabled={!!activeCycle}
+        />
+        <ButtonIconMinutes onClick={handleIncreaseMinutes} type="button">
+          <Plus size={15} />
+        </ButtonIconMinutes>
+      </MinutesAmountContainer>
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
---
+++ src/pages/Home/components/NewCycleForm/styles.ts
@@ -0,0 +1,93 @@
+import styled from 'styled-components'
+
+export const FormContainer = styled.div`
+  width: calc(100% + 1.5rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  flex-wrap: wrap;
+  font-weight: bold;
+`
+
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+
+export const MinutesAmountInput = styled.input`
+  width: 2.5rem;
+
+  &::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    display: none;
+  }
+
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  font-size: 1.125rem;
+  font-weight: bold;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+
+  &:focus {
+    box-shadow: none;
+    border: 1px solid ${(props) => props.theme['green-500']};
+  }
+`
+
+export const MinutesAmountContainer = styled.div`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const ButtonIconMinutes = styled.button`
+  background: transparent;
+  color: ${(props) => props.theme['gray-400']};
+  border: none;
+  width: 1rem;
+  height: 1rem;
+`
